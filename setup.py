c5e388c93f1a89bdd4e963599bea09971d808610
heiseish <dtrnggiang@gmail.com>
2020-05-08 16:45:59 +0800

    Publish version 0.0.6

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     name='kttool',
     cmdclass=cmdclass,
     ext_modules=extensions,
-    version="0.0.5",
+    version="0.0.6",
     description="Kattis command line tool",
     long_description=README,
     long_description_content_type="text/markdown",
